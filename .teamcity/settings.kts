8d934e9f7ce1e2247668a48cb8a5f48c036f29a4
闵杰(Rainbow) <minjie@hypergryph.com>
2026-02-11 14:38:39 +0800

    加了sequence类型和BuildC, 提交后查看buildChain情况

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -27,8 +27,12 @@ To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
 version = "2024.03"
 
 project {
-
     buildType(BuildConfigOfEmptyProject)
+    sequence {
+        build(BuildA)
+        build(BuildB) // BuildB has a snapshot dependency on BuildA
+        build(BuildC) //
+    }
 }
 
 object BuildConfigOfEmptyProject : BuildType({
@@ -46,5 +50,67 @@ object BuildConfigOfEmptyProject : BuildType({
     features {
         perfmon {
         }
+    }
+})
+
+/*
+为Project类添加了一个扩展函数，使我们能够声明sequence。通过使用前面提到的带接收者的Lambda特性，
+我们声明了用作sequence函数参数的代码块将提供Sequence类的上下文
+ */
+class Sequence {
+    val buildTypes = arrayListOf<BuildType>()
+
+    fun build(buildType: BuildType) {
+        buildTypes.add(buildType)
+    }
+}
+
+fun Project.sequence(block: Sequence.()-> Unit){
+    val sequence = Sequence().apply(block)
+    var previous: BuildType? = null
+    // 创建快照依赖
+    for (current in sequence.buildTypes){
+        if (previous != null){
+            current.dependencies.snapshot(previous){}
+        }
+        previous = current
+    }
+    // 对每个构建类型调用buildType函数,以将其包含到当前项目中
+    sequence.buildTypes.forEach(this::buildType)
+}
+
+object BuildA: BuildType({
+    name="BuildA"
+
+    steps {
+        // define the steps needed to publish the artifacts
+    }
+
+    requirements {
+        contains("teamcity.agent.name", "minjie")
+    }
+})
+
+object BuildB: BuildType({
+    name="BuildB"
+
+    steps {
+        // define the steps needed to publish the artifacts
+    }
+
+    requirements {
+        contains("teamcity.agent.name", "minjie")
+    }
+})
+
+object BuildC: BuildType({
+    name="BuildC"
+
+    steps {
+        // define the steps needed to publish the artifacts
+    }
+
+    requirements {
+        contains("teamcity.agent.name", "minjie")
     }
 })
